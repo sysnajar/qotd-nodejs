129108959faf7a237eafdac6e3a4529f4925e882
Owat Owattrakul <sysnajar@gmail.com>
2021-07-26 14:42:15 +0700

    use port 8080

--- Dockerfile
+++ Dockerfile
@@ -8,7 +8,7 @@ RUN npm install
 
 COPY . .
 
-EXPOSE 10000
+EXPOSE 8080
 
 CMD [ "node", "app.js" ]
 
--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-const port = 10000;
+const port = 8080;
 
 
 quotes = [];
